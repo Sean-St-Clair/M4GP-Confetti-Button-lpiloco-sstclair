ff5df7b1b9cebae9ca4b38702ba408aa5f4676ef
Sean-300 <sstclair@uvm.edu>
2023-04-05 18:48:02 -0400

    Added mouse input for the button

--- graphics.cpp
+++ graphics.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <time.h>
 #include <vector>
+
 using namespace std;
 
 GLdouble width, height;
@@ -11,7 +12,8 @@ Button spawn({1, 0, 0}, {100, 100}, 100, 50, "Spawn");
 vector<Quad> confetti;
 
 void spawnConfetti() {
-    confetti.push_back(Quad({rand() % 10 / 10.0, rand() % 10 / 10.0, rand() % 10 / 10.0}, {rand() % (int)width, rand() % (int)height}, 10, 10));
+    confetti.push_back(Quad({rand() % 10 / 10.0, rand() % 10 / 10.0, rand() % 10 / 10.0},
+                            {rand() % (int) width, rand() % (int) height}, 10, 10));
 }
 
 void init() {
@@ -31,27 +33,27 @@ void initGL() {
 void display() {
     // tell OpenGL to use the whole window for drawing
     glViewport(0, 0, width, height);
-    
+
     // do an orthographic parallel projection with the coordinate
     // system set to first quadrant, limited by screen/window size
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(0.0, width, height, 0.0, -1.f, 1.f);
-    
+
     glClear(GL_COLOR_BUFFER_BIT);   // Clear the color buffer with current clearing color
-    
+
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    
+
     /*
      * Draw here
      */
 
-    // TODO: call the Button's draw method.
+    spawn.draw();
 
-    for (const Quad &piece : confetti) {
+    for (const Quad &piece: confetti) {
         piece.draw();
     }
-    
+
     glFlush();  // Render now
 }
 
@@ -62,87 +64,96 @@ void kbd(unsigned char key, int x, int y) {
         glutDestroyWindow(wd);
         exit(0);
     }
-    
+
     glutPostRedisplay();
 }
 
 void kbdS(int key, int x, int y) {
-    switch(key) {
+    switch (key) {
         case GLUT_KEY_DOWN:
-            
+
             break;
         case GLUT_KEY_LEFT:
-            
+
             break;
         case GLUT_KEY_RIGHT:
-            
+
             break;
         case GLUT_KEY_UP:
-            
+
             break;
     }
-    
+
     glutPostRedisplay();
 }
 
 void cursor(int x, int y) {
-    // TODO: If the Button is overlapping with the (x, y) coordinate passed in, call the hover method. Otherwise, call the release method.
-    
+    if (spawn.isOverlapping(x, y)) {
+        spawn.hover();
+    } else {
+        spawn.release();
+    }
     glutPostRedisplay();
 }
 
 // button will be GLUT_LEFT_BUTTON or GLUT_RIGHT_BUTTON
 // state will be GLUT_UP or GLUT_DOWN
 void mouse(int button, int state, int x, int y) {
-    // TODO: If the left button is down and the cursor is overlapping with the Button, call the pressDown method. Otherwise, call the release method.
-
-    // TODO: If the left button is up and the cursor is overlapping with the Button, call spawnConfetti.
-    
+    // If the left button is down and the cursor is overlapping with the Button, call the pressDown method. Otherwise, call the release method.
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && spawn.isOverlapping(x, y)) {
+        spawn.pressDown();
+    } else {
+        spawn.release();
+    }
+    // If the left button is up and the cursor is overlapping with the Button, call spawnConfetti.
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_UP && spawn.isOverlapping(x, y)) {
+        spawnConfetti();
+    }
     glutPostRedisplay();
 }
 
 void timer(int dummy) {
-    
+
     glutPostRedisplay();
     glutTimerFunc(30, timer, dummy);
 }
 
 /* Main function: GLUT runs as a console application starting at main()  */
-int main(int argc, char** argv) {
-    
+int main(int argc, char **argv) {
+
     init();
-    
+
     glutInit(&argc, argv);          // Initialize GLUT
-    
+
     glutInitDisplayMode(GLUT_RGBA);
-    
-    glutInitWindowSize((int)width, (int)height);
+
+    glutInitWindowSize((int) width, (int) height);
     glutInitWindowPosition(100, 200); // Position the window's initial top-left corner
     /* create the window and store the handle to it */
     wd = glutCreateWindow("Confetti!" /* title */ );
-    
+
     // Register callback handler for window re-paint event
     glutDisplayFunc(display);
-    
+
     // Our own OpenGL initialization
     initGL();
-    
+
     // register keyboard press event processing function
     // works for numbers, letters, spacebar, etc.
     glutKeyboardFunc(kbd);
-    
+
     // register special event: function keys, arrows, etc.
     glutSpecialFunc(kbdS);
-    
+
     // handles mouse movement
     glutPassiveMotionFunc(cursor);
-    
+
     // handles mouse click
     glutMouseFunc(mouse);
-    
+
     // handles timer
     glutTimerFunc(0, timer, 0);
-    
+
     // Enter the event-processing loop
     glutMainLoop();
     return 0;
